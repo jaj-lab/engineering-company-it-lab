8c11f0d288c0de031ff2a9440cdbcccb58e837c6
jajai-moto <jajai-moto@gmail.com>
2026-08-27 16:19:46 +0300

    feat: Created and mapped Lambda to S3/SQS flow

--- cloud/lambda/document-processor/index.py
+++ cloud/lambda/document-processor/index.py
@@ -1,8 +1,23 @@
+import json
+
+
 def handler(event, context):
     print("document-processor invoked")
-    print(f"event: {event}")
+    print(f"received SQS event: {event}")
+
+    for record in event.get("Records", []):
+        message_body = json.loads(record["body"])
+
+        for s3_record in message_body.get("Records", []):
+            event_name = s3_record["eventName"]
+            bucket = s3_record["s3"]["bucket"]["name"]
+            object_key = s3_record["s3"]["object"]["key"]
+
+            print(f"event: {event_name}")
+            print(f"bucket: {bucket}")
+            print(f"object: {object_key}")
 
     return {
         "statusCode": 200,
-        "body": "document-processor executed successfully"
+        "body": "document processed successfully"
     }
